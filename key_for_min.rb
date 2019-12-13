a87b813761c3c06ea911b0f7ce26adc1d68ff16d
Anupama Gadde <anupamagadde@yahoo.com>
2019-12-13 03:38:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  min = 0
+   cheapest_item = nil
+   arg.collect do |item, value|
+    if value < min || min == 0
+       min = value
+       cheapest_item = item
+    end
+  end
+  cheapest_item
 end
